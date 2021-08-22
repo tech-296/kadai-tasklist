4a66a685057d3d45e6992349d17a3d18aa9e62bc
cue <tech296@hotmail.com>
2021-08-22 09:46:42 +0000

    update login_3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index]
   
-  def index
-    @pagy, @users = pagy(User.order(id: :desc), items: 5)
-  end
-
   def new
     @user = User.new
   end
